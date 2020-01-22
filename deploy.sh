fd75fa16e8b7e1086d008c19de48948ccf1ac5d3
ilham faturachman <faturachman@alterra.id>
2020-01-22 15:00:30 +0700

    travis lagi3

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop flask-back
 docker rm flask-back
-docker rmi woka/containerd:be-latest
-docker run -d --name flask-back -p 5000:5000 woka/containerd:be-latest
+docker rmi woka/containerd:be-latest-2
+docker run -d --name flask-back -p 5000:5000 woka/containerd:be-latest-2
 
